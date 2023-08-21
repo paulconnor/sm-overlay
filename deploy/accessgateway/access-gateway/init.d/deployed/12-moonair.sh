e770f8e936779b345416304107b6c83a160ff414
paulconnor <paul.connor21@gmail.com>
2023-08-21 16:08:33 +1000

    Create 12-moonair.sh

---
+++ deploy/accessgateway/access-gateway/init.d/deployed/12-moonair.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# * 20-moonair.sh -- add moonair Context
+sed -i '/\/Contexts/i \
+		<Context name="moonair">\
+			docBase="moonair"\
+			path="moonair"\
+			enable="yes"\
+		</Context>' /opt/CA/secure-proxy/proxy-engine/conf/server.conf
